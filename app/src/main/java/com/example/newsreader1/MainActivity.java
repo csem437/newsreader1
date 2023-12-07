ad395ff127e688d7140f1a10d022340f7dcd1fab
csem437 <145692350+csem437@users.noreply.github.com>
2023-12-07 12:33:02 -0500

    second commit

--- app/src/main/java/com/example/newsreader1/MainActivity.java
+++ app/src/main/java/com/example/newsreader1/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.newsreader1;
+
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -13,23 +17,21 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private NewsAdapter newsAdapter;
     private ArrayList<NewsArticle> newsList;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.news_list_article);
+        setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.listView);
+        progressBar = findViewById(R.id.progressBar);
+
         newsList = new ArrayList<>();
-        newsAdapter = new NewsAdapter(this, R.layout.news_list_item, newsList);
+        newsAdapter = new NewsAdapter(this, R.layout.news_list_article, newsList);
         listView.setAdapter(newsAdapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                showDetails(newsList.get(position));
-            }
-        });
+        listView.setOnItemClickListener((parent, view, position, id) -> showDetails(newsList.get(position)));
 
         // Fetch news
         new FetchNewsTask().execute("https://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml");
@@ -44,5 +46,38 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private  class FetchNewsTask extends AsyncTask<String, Void, ArrayList<NewsArticle>> {
+        // Implementation of AsyncTask to fetch news in the background
 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Show ProgressBar before starting the task
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected ArrayList<NewsArticle> doInBackground(String... urls) {
+            try {
+                return NewsUtils.fetchNewsData(urls[0]);
+            } catch (Exception e) {
+                Log.e("FetchNewsTask", "Error fetching news data", e);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<NewsArticle> result) {
+            super.onPostExecute(result);
+            // Update UI with the fetched news
+            newsList.clear();
+            if (result != null) {
+                newsList.addAll(result);
+            }
+            newsAdapter.notifyDataSetChanged();
+
+            // Hide progressbar after the task
+            progressBar.setVisibility(View.GONE);
+        }
+    }
 }
